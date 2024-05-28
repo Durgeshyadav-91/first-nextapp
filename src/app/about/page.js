4923f988621fc7eeff8273c27f6e7352d816df82
Durgesh Yadav <durgesh@vkss.tech>
2024-05-28 10:29:27 -0700

    First commit

---
+++ src/app/about/page.js
@@ -0,0 +1,20 @@
+"use client"
+import Link from "next/link";
+import { useRouter } from 'next/navigation'
+
+export default function about(){
+    const router = useRouter();
+
+    let navigate=(name)=>{
+        return(
+            router.push(name)
+        )
+    }
+    return(
+        <div>
+            <h1>Hello About Page</h1>
+            <Link href={"/"}>Go To Home Page</Link><br/><br/>
+            <button onClick={()=>navigate("/")}>Click To Move Home Page</button>
+        </div>
+    )
+}
