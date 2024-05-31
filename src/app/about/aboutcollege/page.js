f82013b160c9b1e43411ef20c7f2b4e30abfd9f3
Durgesh Yadav <durgesh@vkss.tech>
2024-05-31 18:39:10 +0530

    changes

--- src/app/about/aboutcollege/page.js
+++ src/app/about/aboutcollege/page.js
@@ -4,7 +4,8 @@ export default function() {
     return(
         <div>
             <h1>Hellooooooo About College Page</h1>
-            <Link href={"/about"}>Go to about Page</Link>
+            <Link href={"/about"}>Go to about Page</Link><br/><br/>
+            <Link href={"/"}>Back to Home page </Link>
         </div>
     )
 }
--- src/app/about/aboutstudent/page.js
+++ src/app/about/aboutstudent/page.js
@@ -4,6 +4,7 @@ export default function() {
     return(
         <div>
             <h1>Hellooooooo About Student Page</h1>
+            <Link href={"/"}>Back to Home page </Link><br/><br/>
             <Link href={"/about"}>Go to about Page</Link>
         </div>
     )
